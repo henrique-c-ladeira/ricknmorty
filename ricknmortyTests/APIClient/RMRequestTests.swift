0b307d3e2b2ad2dd9f5673d754be9ae8b6c4551c
Henrique <henrique.c.ladeira@gmail.com>
2023-01-15 18:42:53 -0300

    tests: refactor

--- ricknmortyTests/APIClient/RMRequestTests.swift
+++ ricknmortyTests/APIClient/RMRequestTests.swift
@@ -12,7 +12,7 @@ final class RMRequestTests: XCTestCase {
     
 
     func test_string_url_with_2_pathComponents_and_1_query_item() throws {
-        let sut = makeSut(endpoint: RMEndpoint(), pathComponents: ["pathComponent1","pathComponent2"], queryParameters: [URLQueryItem(name: "name", value: "value")])
+        let sut = makeSut(pathComponents: ["pathComponent1","pathComponent2"], queryParameters: [URLQueryItem(name: "name", value: "value")])
         
         let expectedUrlString = "https://rickandmortyapi.com/api/pathComponent1/pathComponent2?name=value"
         
@@ -20,7 +20,7 @@ final class RMRequestTests: XCTestCase {
     }
     
     func test_string_url_with_1_pathComponents_and_2_query_item() throws {
-        let sut = makeSut(endpoint: RMEndpoint(), pathComponents: ["pathComponent1"],
+        let sut = makeSut(pathComponents: ["pathComponent1"],
                           queryParameters: [URLQueryItem(name: "name1", value: "value1"), URLQueryItem(name: "name2", value: "value2")])
         
         let expectedUrlString = "https://rickandmortyapi.com/api/pathComponent1?name1=value1&name2=value2"
@@ -28,15 +28,14 @@ final class RMRequestTests: XCTestCase {
     }
     
     func test_string_url_with_0_pathComponents_and_2_query_item() throws {
-        let sut = makeSut(endpoint: RMEndpoint(), pathComponents: [""],
-                          queryParameters: [URLQueryItem(name: "name1", value: "value1"), URLQueryItem(name: "name2", value: "value2")])
+        let sut = makeSut(queryParameters: [URLQueryItem(name: "name1", value: "value1"), URLQueryItem(name: "name2", value: "value2")])
         
         let expectedUrlString = "https://rickandmortyapi.com/api?name1=value1&name2=value2"
         XCTAssertEqual(sut.url!.absoluteString, expectedUrlString)
     }
     
     // Mark - Utils
-    private func makeSut(endpoint: RMEndpoint, pathComponents: [String], queryParameters: [URLQueryItem]) -> RMRequest {
+    private func makeSut(endpoint: RMEndpoint = RMEndpoint(), pathComponents: [String] = [], queryParameters: [URLQueryItem]) -> RMRequest {
         let sut = RMRequest(endpoint: endpoint, pathComponents: pathComponents, queryParameters: queryParameters)
         return sut
     }
